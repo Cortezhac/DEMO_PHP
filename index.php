6f1f81e196fdacbf903c51626517375bc6cbc076
Irvin Cortez <48500847+Cortezhac@users.noreply.github.com>
2020-05-08 14:43:02 -0600

    parrafo agregado a index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DEMO PHP</title>
+</head>
+<body>
+    <?php 
+        echo "Hola mundo";
+    ?>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolores aut iure ducimus quos quod qui facere tempore quidem corrupti quo deleniti reiciendis necessitatibus quisquam, voluptatibus explicabo autem repellendus cupiditate.</p>
+</body>
+</html>
