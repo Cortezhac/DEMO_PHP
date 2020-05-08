41678446e724446cf240f973db5184d83a359883
Irvin Cortez <48500847+Cortezhac@users.noreply.github.com>
2020-05-08 15:50:32 -0600

    Creacion del directorio ITCA y PHP

---
+++ ITCA/cortez.php
@@ -0,0 +1,16 @@
+<p>
+	<?php
+
+		echo "Segundo documento escrito en nano, lo malo de esta cosa es que no puedo usar los atajos de toda la vida o almenos no funcionan igual :v<br>";
+		echo "Yo: intento usar control + Z para revertir lo que hago<br>";
+		echo "El nano: aqui no hacemos eso <br>";
+		echo "Yo: Entonces como revierto lo que hago <br>";
+		echo "El nano: no lo se tu dime <br>";
+		echo "Yo: sera este el fin del hombre arania<br>";
+		echo "MASAKA? no puedo usar la N pero con virbulilla esto se esta volviendo cada vez mas triste <br>";
+		echo "Quien necesita la N pero con viurbulilla puedo vivir sin ella :'v<br>";
+		echo "Yo: Bueno y ahora como salgo de aqui?";
+		echo "El nano: Buena pregunta?";
+		echo "to be continue........";
+	?>
+</p>
---
+++ ITCA/irvin.php
@@ -0,0 +1,6 @@
+<?php 
+	echo "Experimetando el trabajo en consula usando Gitbash y editando el documento en nano desu XD";
+	echo "<p>";
+	echo "contenido random que estoy agregando que sirve de relleno pero no como el de NARUTO";
+	echo "<?p>";
+?>
